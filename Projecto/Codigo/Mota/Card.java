13c5d0a7c04712fae4b3ca6585b94486d1843118
Ruben Tadeia <Ruben Tadeia>
2017-04-25 18:18:20 +0100

    After Bruno Left

--- Projecto/Codigo/Mota/Card.java
+++ Projecto/Codigo/Mota/Card.java
@@ -1,28 +1,72 @@
 package VideoPoker;
-import java.lang.String;
 
 public class Card {
-	private String rank;
 	private String suit;
+	private int value;
 	
-	Card(String[] k){
-		this.rank = k[0];
-		this.suit = k[1];
+	public Card(String card){
+		
+		if (card.substring(1).equals("H") || card.substring(1).equals("S") ||
+				card.substring(1).equals("D") || card.substring(1).equals("C"))
+		{
+			this.suit = card.substring(1);
+		}
+		else
+		{
+			System.out.println(
+					"Wrong type of suit... "
+					+ "Strong with your Deck the force is not!");
+		}
+		
+		String rank = card.substring(0,1);
+		
+		if (rank.equals("T"))
+		{
+			this.value = 10;	
+		}
+		else if (rank.equals("J")){
+			this.value = 11;
+		}
+		else if (rank.equals("Q")){
+			this.value = 12;
+		}
+		else if (rank.equals("K")){
+			this.value = 13;
+		}
+		else if (rank.equals("A")){
+			this.value = 14;
+		}
+		else if (Integer.parseInt(rank) >= 2 && 
+				Integer.parseInt(rank) <= 9)
+		{
+			this.value = Integer.parseInt(rank);	
+		}
+		else
+		{
+			System.out.println(
+					"Wront type of rank... "
+					+ "Strong with your Deck the force is not!");	
+		}
 	}
-	public String getRank(){
-		return this.rank;
+
+	public String getSuit() {
+		return suit;
+	}
+
+	public void setSuit(String suit) {
+		this.suit = suit;
 	}
 	
-	public String getSuit(){
-		return this.suit;
+	public void printCard(){
+		System.out.println("Card Value - " + this.value + 
+				" And the Suit - " + this.suit);
 	}
-	
-	public void setCard(String[] k){
-		this.rank = k[0];
-		this.suit = k[1];
+
+	public int getValue() {
+		return value;
 	}
-	@Override
-	public String toString(){
-		return this.rank +this.suit;
-	} 
+
+	public void setValue(int value) {
+		this.value = value;
+	}
 }
--- Projecto/Codigo/Mota/Deck.java
+++ Projecto/Codigo/Mota/Deck.java
@@ -1,53 +1,57 @@
 package VideoPoker;
-import java.util.Random;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Deck {
-	Card[] deck;
-	int head;
-	
-	public Deck(int size){
-		this.deck = new Card[size];
-		this.head = 0;
+	private List<Card> deck = new ArrayList<>();
+	private int topCard;
+	public Deck(){
+		/** For a default Deck with 52 Cards */
+		this.topCard = 0;
+		char rank[] = {'2', '3', '4', '5', '6', '7', '8', '9', 'T'
+				, 'J', 'Q','K','A'};
+		
+		char suit[] = {'H','C','S','D'};
+		
+		String auxiliarCard;
+		
+		for (char i : suit){
+			for(char j : rank){
+				auxiliarCard = Character.toString(j) + Character.toString(i);
+				Card card = new Card(auxiliarCard);
+				deck.add(card);
+			}
+		}
 	}
 	
-	public Card getNewCard(){
-		Card t = this.deck[head];
-		this.head++;
-		return t;
+	public Deck(ArrayList<String> cardsVector){
+		/** For a deck considering that a file is provided */
+		for(String s: cardsVector){
+			Card card = new Card(s);
+			deck.add(card);
+			System.out.println("that's the card " +s);
+		}
 	}
 	
-	public void insertNewCard(String[] k){
-		this.deck[head].setCard(k); 
-		this.head++;
+	public List<Card> getDeck() {
+		return deck;
+	}
+
+	public Card getCard(int pila) {
+		Card turn = deck.get( pila);
+		return turn;
+	}
+	
+	public void setDeck(List<Card> deck) {
+		this.deck = deck;
 	}
 	
 	public void shuffle(){
-		//index declaration
-		int i0=0,i1=0;
-		//Random number 
-		Random ran = new Random();
-		//Strings to contain card description
-		String[] temp = new String[2];
-		String[] temp1 = new String[2];
-		//Shuffle (Can be changed to a number in order to shuffle a determined number of times)
-		for (int idx = 0; idx <= this.deck.length; ++idx){
-		      i0 = ran.nextInt(this.deck.length);
-		      i1 = ran.nextInt(this.deck.length);
-		      //Card i0
-		      temp[0] = deck[i0].getRank();
-		      temp[1] = deck[i0].getSuit();
-		      //Card i1
-		      temp1[0] = deck[i1].getRank();
-		      temp1[1] = deck[i1].getSuit();
-		      //Switch
-		      deck[i0].setCard(temp1);
-		      deck[i1].setCard(temp);
-		    }
+		Collections.shuffle(this.deck);
 	}
 	
-	public void printDeck(){
-		for(int idx = 0; idx <= this.deck.length; ++idx){
-			System.out.println("Card"+deck[idx].toString());
-		}
-	}
+
 }
+
