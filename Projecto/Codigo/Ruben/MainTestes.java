c2e0111d8110e15aca258e477798773fb8d69665
Ruben Tadeia <Ruben Tadeia>
2017-05-13 03:01:31 +0100

    Strategy working on debugs

--- Projecto/Codigo/Ruben/MainTestes.java
+++ Projecto/Codigo/Ruben/MainTestes.java
@@ -6,7 +6,7 @@ public class MainTestes {
 	public static void main(String[] args){
 		
 		ArrayList<String> cardsVector = new ArrayList<String>();;
-		FileHandler fileHandler = new FileHandler("./src/videoPoker/newDeck.txt");
+		FileHandler fileHandler = new FileHandler("./src/videoPoker/hardHands.txt");
 		
 		cardsVector = fileHandler.getCardsVector();
 		Deck deck = new Deck(cardsVector);
@@ -17,6 +17,17 @@ public class MainTestes {
 		
 		Strategy strategy = new Strategy(hand.getHand(),analyser.getCheckerResult());
 		
+		hand.printHand();
+		
+		strategy.holdPositionCorrector();
+		
+		for (Card b : strategy.getHandSorted()){
+			System.out.println(b.getRank()+b.getSuit());
+		}
+		
+		for (int a : strategy.getIndicesToHold()){
+			System.out.println("a = " + a);
+		}
 		
 	}		
 }
--- Projecto/Codigo/Ruben/Strategy.java
+++ Projecto/Codigo/Ruben/Strategy.java
@@ -42,10 +42,40 @@ public class Strategy {
 		gatherIndexToHold();
 		
 		for(int i = 0; i < this.indicesToHold.size(); i++){
-			handSorted.get(indicesToHold.get(i));
-			// matching between hands
 			
+			/* What this line does is gets the element of the Sorted and that is in the indicesToHold
+			 * Then gets the index of the same element in the handUnsorted 
+			 */
+			this.indicesToHold.set(i, handUnsorted.indexOf(handSorted.get(indicesToHold.get(i))));
 		}
+	}
+	
+	public void setIndicesToHold(List<Integer> indicesToHold) {
+		this.indicesToHold = indicesToHold;
+	}
+
+	private boolean checkThreeAces()
+	/** This method checks if there is at least a three of a kind */
+	{
+		int checkPair = 1;
+		/* Need a new name for this variable */
+		int iterationValue = handSorted.get(0).getValue();
+		
+		for (int i = 1; i < this.handSorted.size(); i++)
+		{
+			if(iterationValue == handSorted.get(i).getValue() && iterationValue == 14){
+				checkPair++;
+			}
+			else{
+				checkPair = 1;
+			}
+			if (checkPair == 3)
+			{
+				return true;
+			}
+			iterationValue = handSorted.get(i).getValue();
+		}
+		return false;
 	}
 	
 	/* This method will gather the indices
@@ -55,7 +85,7 @@ public class Strategy {
 		
 		this.indicesToHold.clear();
 		
-		/** Case 1 - Straight Flush, four of a kind, royal Flush*/
+		/** Case 1 - Straight Flush, four of a kind, royal Flush */
 		if (analyserResult == 8){ /* Royal flush */
 			holdAll();
 			return;
@@ -80,7 +110,83 @@ public class Strategy {
 		
 		/** Case 2 - 4 to a royal Flush */
 		
+		/** Case 3 - Three aces */
 		
+		if(checkThreeAces()){
+			for(Card d : handSorted){
+				if (d.getValue() == 14){
+					this.indicesToHold.add(handSorted.indexOf(d));
+					this.indicesToHold.add(handSorted.indexOf(d)+1);
+					this.indicesToHold.add(handSorted.indexOf(d)+2);
+					return;
+				}
+			}
+		}
+		
+		/** Case 4 - Straight, flush, full house */
+		
+		
+		
+		/** Case 5 - Three of a kind (except aces) */
+		
+		/** Case 6 - 4 to a straight flush */
+		
+		/** Case 7 - Two pair */
+		
+		/** Case 8 - High pair */
+		
+		/** Case 9 - 4 to a Flush */
+		
+		/** Case 10 - 3 to a royal Flush */
+		
+		/** Case 11 - 4 to an outside straight */
+		
+		/** Case 12 - Low pair */
+		
+		/** Case 13 - AKQJ unsuited */
+		
+		/** Case 14 - 3 to a straight Flush (type 1) */
+		
+		/** Case 15 - 4 to an inside straight with 3 high cards */
+		
+		/** Case 16 - QJ suited */
+		
+		/** Case 17 - 3 to a flush with 2 high cards */
+		
+		/** Case 18 - 2 suited high cards */ 
+		
+		/** Case 19 - 4 to an inside straight with 2 high cards */
+		
+		/** Case 20 - 3 to a straight flush (type 2) */
+		
+		/** Case 21 - 4 to an inside straight with 1 high card */
+		
+		/** Case 22 - KQJ unsuited */
+		
+		/** Case 23 - JT suited */
+		
+		/** Case 24 - QJ unsuited */
+		
+		/** Case 25 - 3 to a flush with 1 high card */
+		
+		/** Case 26 - QT suited */
+		
+		/** Case 27 - 3 to a straight Flush (type 3) */
+		
+		/** Case 28 - KQ, KJ unsuited */
+		
+		/** Case 29 - Ace */
+		
+		/** Case 30 - KT suited */
+		
+		/** Case 31 - Jack, Queen or King */
+		
+		/** Case 32 - 4 to an inside straight with no high cards */
+		
+		/** Case 33 - 3 to a Flush with no high cards */
+		
+		/** Case 34 - Discard everything */
+		return;
 	}
 	
 	private void holdAll(){
